a59ab53bf0bbf40f868909a4a09a7b4078d4162d
darrenrector91 <darrenrector91@gmail.com>
2018-04-10 23:31:17 -0500

    views setup

---
+++ server/public/scripts/services/user.service.js
@@ -0,0 +1,7 @@
+myApp.service('UserService', ['$http', '$location', '$mdDialog', function ($http, $location, $mdDialog, $ngMap) {
+    // console.log('UserService Loaded');
+    var self = this;
+
+    self.userObject = {};
+
+}]);
