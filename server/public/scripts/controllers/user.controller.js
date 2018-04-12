10751f854fa59cee543f79f66345adfe9ba078ac
darrenrector91 <darrenrector91@gmail.com>
2018-04-11 22:30:41 -0500

    chartjs added

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -3,11 +3,17 @@ myApp.controller('UserController', ['UserService', '$http', function (UserServic
   var self = this;
   self.userService = UserService;
   self.userObject = UserService.userObject;
+  self.espTemp = {}
+  self.tableTemp = {
+    list: []
+  }
 
-  /* Line Graph */
+  /* Graph */
   var ctx = document.getElementById('lineChart').getContext('2d');
+  console.log();
+
   var chart = new Chart(ctx, {
-    // The type of chart we want to create
+    // Chart type
     type: 'line',
 
     // dataset
@@ -36,14 +42,14 @@ myApp.controller('UserController', ['UserService', '$http', function (UserServic
           scaleLabel: {
             display: true,
             labelString: 'Temp in ˚F',
-            fontSize: 18
+            fontSize: 20
           }
         }],
         xAxes: [{
           scaleLabel: {
             display: true,
             labelString: 'Date/Time',
-            fontSize: 18
+            fontSize: 20
           }
         }]
       },
@@ -54,7 +60,39 @@ myApp.controller('UserController', ['UserService', '$http', function (UserServic
     }
   });
 
-  self.getIOTdata = function (){
+  self.getIOTdata = function () {
     UserService.getTemp();
+    console.log(self.getIOTdata);
+
   }
+
+
+
+  self.getTemp = function () {
+    $http({
+      method: 'GET',
+      url: 'https://api.thingspeak.com/channels/470875/feeds.json?api_key=BVCBAPTG0YM0STEA&results=5'
+    }).then(function (response) {
+      //The last 5 temp readings
+      self.tableTemp.list = response.data.feeds;
+
+      //The last temp reading
+      // chart.chart.config.data.labels = response.data.feeds[4].created_at;
+      chart.chart.config.data.datasets[0].data = response.data.feeds[4].field1;
+      console.log(chart.chart.config.data.datasets[0].data);
+      
+      console.log(response.data.feeds[4].field1);
+      console.log(response.data.feeds[4].created_at);
+
+      chart.update();
+
+      // self.espTemp = response.data.feeds[4].field1;
+      //The last 5 temp readings console
+      console.log(self.tableTemp);
+      console.log(self.tableTemp.list);
+      //The last temp reading console
+      // console.log('temp', self.espTemp);
+    })
+  }
+  self.getTemp();
 }]);
--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -3,26 +3,27 @@ myApp.service('UserService', ['$http', '$location', function ($http, $location) 
     var self = this;
 
     self.userObject = {};
-    self.espTemp = {}
-    self.tableTemp = {
-        list: []
-    }
+    // self.espTemp = {}
+    // self.tableTemp = {
+    //     list: []
+    // }
 
-    self.getTemp = function () {
-        $http({
-            method: 'GET',
-            url: 'https://api.thingspeak.com/channels/470875/feeds.json?api_key=BVCBAPTG0YM0STEA&results=5'
-        }).then(function (response) {
-            //The last 5 temp readings
-            self.tableTemp.list = response.data.feeds;
-            //The last temo reading
-            self.espTemp = response.data.feeds[4].field1;
-            //The last 5 temp readings console
-            console.log(self.tableTemp);
-            console.log(self.tableTemp.list);
-            //The last temp reading console
-            console.log('temp', self.espTemp);
-        })
-    }
-    self.getTemp();
+    // self.getTemp = function () {
+    //     $http({
+    //         method: 'GET',
+    //         url: 'https://api.thingspeak.com/channels/470875/feeds.json?api_key=BVCBAPTG0YM0STEA&results=5'
+    //     }).then(function (response) {
+    //         //The last 5 temp readings
+    //         self.tableTemp.list = response.data.feeds;
+    //         //The last temo reading
+    //         chart.chart.config.data.datasets[0].data = response.data.feeds[4].field1;
+    //         // self.espTemp = response.data.feeds[4].field1;
+    //         //The last 5 temp readings console
+    //         console.log(self.tableTemp);
+    //         console.log(self.tableTemp.list);
+    //         //The last temp reading console
+    //         console.log('temp', self.espTemp);
+    //     })
+    // }
+    // self.getTemp();
 }]);
