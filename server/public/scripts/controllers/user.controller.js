0a6bdac5887696b2a91ca52b62153720ff825f8a
darrenrector91 <darrenrector91@gmail.com>
2018-04-19 20:53:40 -0500

    chart up and running

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,16 +1,24 @@
 myApp.controller('UserController', ['UserService', '$http', function (UserService, $http) {
-  console.log('UserController created');
+  // console.log('UserController created');
   var self = this;
   self.userService = UserService;
   self.userObject = UserService.userObject;
-  self.espTemp = {}
-  self.tableTemp = {
-    list: []
-  
-  }
-  let results = [];
+
+  let data = [];
+  console.log(data);
+
   let labels = [];
-  
+  console.log(labels);
+
+
+  let num = ['20', '22'];
+  let sum = 0;
+  for (i = 0; i < num.length; i++) {
+    sum += parseInt(num[i]);
+    console.log('sum', sum);
+  }
+
+
 
   var ctx = document.getElementById('lineChart').getContext('2d');
 
@@ -20,82 +28,98 @@ myApp.controller('UserController', ['UserService', '$http', function (UserServic
     data: {
       labels: labels,
       datasets: [{
-        label: "Office Temperature",
-        backgroundColor: 'rgb(164, 198, 57)',
-        borderColor: 'rgb(164, 198, 57)',
-        data: results,
-        fill: false
+        label: "Office",
+        backgroundColor: '#ff0000',
+        borderColor: '#ff0000',
+        data: data,
+        fill: false,
       }]
-      
     },
 
     options: {
       title: {
         display: true,
-        text: 'Office Temp',
+        text: 'Office',
         position: 'top',
         fontSize: 24,
-        fontColor: '#000000'
+        fontColor: '#000'
       },
       scales: {
         yAxes: [{
+          ticks: {
+            fontSize: 15,
+            fontColor: '#000',
+          },
           scaleLabel: {
             display: true,
-            labelString: 'Temp in ˚F',
+            // labelString: 'Temp in ˚F',
             fontSize: 20
           }
         }],
         xAxes: [{
+          ticks: {
+            fontSize: 20,
+            fontColor: '#000'
+          },
           scaleLabel: {
             display: true,
-            labelString: 'Date/Time',
+            // labelString: 'Date/Time',
             fontSize: 20
           }
         }]
       },
       legend: {
-        display: false,
+        display: true,
         position: 'bottom'
       }
     }
-    
+
   });
 
-  // self.getIOTdata = function () {
-  //   UserService.getTemp();
-  //   console.log(self.getIOTdata);
 
-  // }
+
   /* Graph */
   self.getTemp = function () {
     $http({
       method: 'GET',
-      url: 'https://api.thingspeak.com/channels/470875/fields/1.json?api_key=BVCBAPTG0YM0STEA&results=20'
+      url: 'https://api.thingspeak.com/channels/470875/fields/1.json?api_key=BVCBAPTG0YM0STEA&results=5'
     }).then(function (response) {
-      console.log(response.data.feeds);
-      
-      // let labels = [];
-      for (var i = 0; i < response.data.feeds.length; i++) {
-        labels.push(response.data.feeds[i].created_at);
+      let jsonData = response.data.feeds;
+      // console.log('self.espData', self.espData);
+      // console.log(jsonData);
+
+      for (var i = 0; i < jsonData.length; i++) {
+        labels.push(jsonData[i].created_at);
+        // console.log(jsonData[i].created_at);
+        
+
       }
       console.log(labels);
 
-      for (var i = 0; i < response.data.feeds.length; i++) {
-        results.push(response.data.feeds[i].field1);
+      // let sum = {};
+      let avg = {};
+      let arrLength = jsonData.length;
+
+      for (var i = 0; i < arrLength; i++) {
+        data.push(jsonData[i].field1);
+        console.log(jsonData[i].field1);
+
       }
-      console.log(results);
-      
+      console.log('temps', data);
+      element = 0;
+      for (let i = 0; i < data.length; i++) {
+        // element += data[i];
+        let z = console.log(parseInt(data[i]));
+      }
       chart.update();
     })
   }
   self.getTemp();
 
-  Date.prototype.formatMMDDYYYY = function () {
-    return (this.getMonth() + 1) +
-      "/" + this.getDate() +
-      "/" + this.getFullYear();
-  }
-
-
+  // Date.prototype.formatMMDDYYYY = function () {
+  //   return (this.getMonth() + 1) +
+  //     "/" + this.getDate() +
+  //     "/" + this.getFullYear();
+  // }
 
 }]);
