c9339a376e151041b64c7556615d384ab1c68bcb
darrenrector91 <darrenrector91@gmail.com>
2018-07-28 15:01:27 -0500

    getting data back from Thingspeak

--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -11,18 +11,23 @@ myApp.service('UserService', ['$http', '$location', function ($http, $location) 
     self.getTemp = function () {
         $http({
             method: 'GET',
-            url: 'GET https://api.thingspeak.com/channels/470875/fields/1.json?api_key=A59ZTI1P9ONS0ZRF&results=5'
+            url: 'https://api.thingspeak.com/channels/470875/fields/1.json?api_key=A59ZTI1P9ONS0ZRF&results=5'
         }).then(function (response) {
             //The last 5 temp readings
             self.tableTemp.list = response.data.feeds;
-            //The last temo reading
+            //The last temp reading
             self.espTemp = response.data.feeds[4].field1;
             //The last 5 temp readings console
             console.log(self.tableTemp);
-            console.log(self.tableTemp.list);
+            console.log('Thingspeak data: ',self.tableTemp.list);
             //The last temp reading console
             console.log('temp', self.espTemp);
         })
+        .catch(function (response) {
+            console.log(response);
+            
+            console.log('error on get request');
+        });
     }
     self.getTemp();
 }]);
